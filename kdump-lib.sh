041f19ebcf89dd41b64910440edea1a26e30bd49
Coiby Xu <coxu@redhat.com>
2022-08-18 09:29:09 +0800

    Eliminate shellcheck SC2034 warnings
    DEFAULT_SSHKEY, and FENCE_KDUMP_SEND are used by whoever sources
    kdump-lib-initramfs.sh and KDUMP_INITRD is used by kdumpctl. So they are
    false positives.
    
    FENCE_KDUMP_CONFIG_FILE is only used by kdumpctl and
    dracut-module-setup.sh both of which source kdump-lib.sh so define
    FENCE_KDUMP_CONFIG_FILE in kdump-lib.sh instead.
    
    dracut_args is unused.
    
    Signed-off-by: Coiby Xu <coxu@redhat.com>

--- kdump-lib.sh
+++ kdump-lib.sh
@@ -8,8 +8,10 @@ else
 	. /lib/kdump/kdump-lib-initramfs.sh
 fi
 
+# shellcheck disable=SC2034
 FADUMP_ENABLED_SYS_NODE="/sys/kernel/fadump/enabled"
 FADUMP_REGISTER_SYS_NODE="/sys/kernel/fadump/registered"
+# shellcheck disable=SC2034
 FADUMP_APPEND_ARGS_SYS_NODE="/sys/kernel/fadump/bootargs_append"
 
 is_uki()
@@ -663,8 +665,10 @@ prepare_kdump_bootinfo()
 	if [[ ! -w $KDUMP_BOOTDIR ]]; then
 		var_target_initrd_dir="/var/lib/kdump"
 		mkdir -p "$var_target_initrd_dir"
+		# shellcheck disable=SC2034 # KDUMP_INITRD is used by kdumpctl
 		KDUMP_INITRD="$var_target_initrd_dir/$kdump_initrd_base"
 	else
+		# shellcheck disable=SC2034 # KDUMP_INITRD is used by kdumpctl
 		KDUMP_INITRD="$KDUMP_BOOTDIR/$kdump_initrd_base"
 	fi
 }
