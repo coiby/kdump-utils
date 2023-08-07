0df23d05ad7b67fcb37948140fad5649b5a43c2e
Coiby Xu <coxu@redhat.com>
2023-08-07 15:19:36 +0800

    Support dumping to a LUKS-encrypted target
    Based on the new kernel feature that dm-crypt keys can persist for the
    kdump kernel [1], this patch which is adapted from [2]
    1) ask the 1st kernel to save a copy of the LUKS volume keys
    2) ask the kdump kernel to add the copy of the LUKS volume keys to
       specified keyring and then use --volume-key-keyring the unlock the
       LUKS device.
    
    [1] https://github.com/coiby/linux/blob/dm_crypt_v15/Documentation/ABI/testing/crash_dm_crypt_keys
    [2] https://lists.fedorahosted.org/archives/list/kexec@lists.fedoraproject.org/message/Y3KUSJQPN3JHUUC2FPIK7H4HTSX2TUCX/
    
    Signed-off-by: Coiby Xu <coxu@redhat.com>

--- dracut/99kdumpbase/module-setup.sh
+++ dracut/99kdumpbase/module-setup.sh
@@ -1072,6 +1072,48 @@ ForwardToConsole=yes
 EOF
 }
 
+kdump_check_crypt_targets() {
+    local _luks_dev _count _devuuid _key_desc
+    declare -a _luks_devs
+
+    _luks_devs=($(get_all_kdump_crypt_dev))
+    _count=${#_luks_devs[@]}
+
+    if [[ $_count -lt 1 ]]; then
+        return
+    fi
+
+    # This overrides behaviour of 90crypt
+    inst cryptsetup
+    instmods dm_crypt
+
+    echo >"$initdir/etc/cmdline.d/90crypt.conf"
+    echo >"$initdir/etc/crypttab"
+    echo >"${initdir}/sbin/crypt-run-generator"
+
+    # configfs is mounted after dracut pre-udev hook
+    inst_hook initqueue 20 "$moddir/kexec-crypt-setup.sh"
+
+    mkdir -p $hookdir/initqueue/finished
+
+    for _luks_dev in "${_luks_devs[@]}"; do
+        _devuuid=$(maj_min_to_uuid "$_luks_dev")
+        _key_desc=cryptsetup:$_devuuid
+
+        mkdir -p "${initdir}/etc/udev/rules.d/"
+        {
+            printf -- 'ENV{ID_FS_UUID}=="%s", ' "$_devuuid"
+            printf -- 'RUN+="/sbin/initqueue  --settled --unique --onetime '
+            printf -- '--name kdump-crypt-target-%%k %s ' "$(command -v cryptsetup)"
+            printf -- 'luksOpen --volume-key-keyring %s $env{DEVNAME} %s"\n' "%%user:$_key_desc" "luks-$_devuuid"
+        } >>"${initdir}/etc/udev/rules.d/70-luks-kdump.rules"
+
+    done
+
+
+    dracut_need_initqueue
+}
+
 install() {
     declare -A unique_netifs ovs_unique_netifs ipv4_usage ipv6_usage
 
@@ -1120,6 +1162,8 @@ install() {
     # target. Ideally all this should be pushed into dracut iscsi module
     # at some point of time.
     kdump_check_iscsi_targets
+
+    kdump_check_crypt_targets
 
     kdump_install_systemd_conf
 
