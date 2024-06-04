84187a54bc59af51b3547a29a22c79276f056daa
Coiby Xu <coxu@redhat.com>
2024-06-04 15:51:47 +0800

    LUKS: make /usr writable
    Since commit ffc1ec73b3 ("pid1: add ProtectSystem= as system-wide
    configuration, and default it to true in the initrd"), systemd makes
    /usr read-only by default and it will cause dracut to not wait for the
    LUKS-encrypted devices to be unlocked,
    
        dracut-cmdline[296]: mv: inter-device move failed: '/tmp/294-daemon-reload.sh' to '/lib/dracut/hooks/initqueue/daemon-reload.sh'; unable to remove target: Read-only file syste
    
        dracut-cmdline[294]: /sbin/initqueue: line 71: /lib/dracut/hooks/initqueue/work: Read-only file system
        dracut-cmdline[221]: /lib/dracut-dev-lib.sh: line 118: /lib/dracut/hooks/initqueue/finished/devexists-\x2fdev\x2fmyvg\x2fluks_lv.sh: Read-only file system
        dracut-cmdline[221]: /lib/dracut-dev-lib.sh: line 103: /lib/dracut/hooks/emergency/80-\x2fdev\x2fmyvg\x2fluks_lv.sh: Read-only file system
    
    Fix the above issue by making /usr writable.
    
    Signed-off-by: Coiby Xu <coxu@redhat.com>

--- dracut/99kdumpbase/module-setup.sh
+++ dracut/99kdumpbase/module-setup.sh
@@ -1110,6 +1110,12 @@ kdump_check_crypt_targets() {
 
     done
 
+    # latest systemd makes /usr read-only by default
+    mkdir -p "${initdir}/etc/systemd/system.conf.d"
+    {
+        echo "[Manager]"
+        echo "ProtectSystem=false"
+    } >>"${initdir}/etc/systemd/system.conf.d/kdump_luks.conf"
 
     dracut_need_initqueue
 }
